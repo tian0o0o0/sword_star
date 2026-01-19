4fc1371307705688cda56c506d19e48540375b8f
tian0o0o0 <tiantian067675@gmail.com>
2026-01-19 14:44:42 +0700

    wd

--- sword_star_main.cpp
+++ sword_star_main.cpp
@@ -1,6 +1,6 @@
-
+#include "SFML/Audio.hpp"
+#include "SFML/Graphics.hpp"
 #include <iostream>
 int main(){
-    std::cout<<"hi";
-    return 0;
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
 }
