cc0c87950e51e9d4d111663c1bd4d07b9592c8a9
tian0o0o0 <tiantian067675@gmail.com>
2026-01-19 15:28:05 +0700

    start

--- sword_star_main.cpp
+++ sword_star_main.cpp
@@ -1,42 +1,7 @@
 #include <SFML/Graphics.hpp>
  
-//& ".\library\SFML-3.0.2\compiler\mingw64\bin\g++.exe" -g ".\sword_star_main.cpp" -o ".\sword_star_main.exe" -I".\library\SFML-3.0.2\include" -L".\library\SFML-3.0.2\lib" -lsfml-graphics -lsfml-window -lsfml-system; $env:PATH = ".\library\SFML-3.0.2\bin;$env:PATH"; & ".\sword_star_main.exe"
 
 int main()
 {
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
 
-    // Create a simple shape to display
-    sf::CircleShape circle(100.f);
-    circle.setFillColor(sf::Color::Green);
-    circle.setPosition({350.f, 250.f});
- 
-    // Start the game loop
-    while (window.isOpen())
-    {
-        // Process events
-        while (const std::optional event = window.pollEvent())
-        {
-            // Close window: exit (X button)
-            if (event->is<sf::Event::Closed>())
-                window.close();
-            
-            // Also close with Escape key
-            if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
-            {
-                if (keyPressed->code == sf::Keyboard::Key::Escape)
-                    window.close();
-            }
-        }
- 
-        // Clear screen
-        window.clear();
- 
-        // Draw the circle
-        window.draw(circle);
- 
-        // Update the window
-        window.display();
-    }
 }
