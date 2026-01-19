07c5a70e01c625937d1d10679ea940d6486fa154
tian0o0o0 <tiantian067675@gmail.com>
2026-01-19 22:09:59 +0700

    w

--- sword_star_main.cpp
+++ sword_star_main.cpp
@@ -1,7 +1,18 @@
 #include <SFML/Graphics.hpp>
- 
+#include<iostream>
 
 int main()
 {
+    sf::RenderWindow screen(sf::VideoMode::getDesktopMode(),"Sword star");
+
+    screen.setFramerateLimit(165);
+    while(screen.isOpen()){
+        while(const std::optional event = screen.pollEvent()){
+            if(event->is<sf::Event::Closed>())
+            screen.close();
+        }
+    screen.clear();
+    screen.display();
+    }
 
 }
